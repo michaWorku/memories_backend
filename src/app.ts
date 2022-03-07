b5bf5ea366dd359f2f10235c154f559cc8fd093d
michaworku <mikias27worku@gmail.com>
2022-03-07 17:18:07 +0300

    user routes, controller and authorization middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import { Express, Request, Response } from 'express'
 import config from 'config'
 import cors from 'cors'
 import log from './logger'
 import connect from './db/connect'
 //import routes from './routes'
 import apiErrorHandler from './controllers/error.controller'
+import userRouter from './routes/user.routes'
 
 const PORT = (config.get('PORT') || 3001) as number
 
@@ -18,6 +20,12 @@ app.use(cors())
 app.listen(PORT, async () => {
   log.info(`Server is running at ${PORT}`)
   await connect()
-  //routes(app)
-  app.use(apiErrorHandler)
+  routes(app)
+  
 })
+
+app.get('/api', (req: Request, res: Response) =>
+ res.send('Welcome to memories-app')
+)
+app.use('/api/users', userRouter)
+app.use(apiErrorHandler)
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import {Users} from '../controllers/users'
+import { signup, signin, getUser } from './controllers/user.controller'
+import requiresUser from './middleware/requiresUser'
+const router = express.Router();
+
+// Auth routes
+router.post('/signin', signin);
+router.post('/signup', signup);
+
+// User routes
+router.get('/users/:id',requiresUser, getUser)
+
+
+export default router;
